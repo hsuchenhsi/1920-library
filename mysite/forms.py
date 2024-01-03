983db2126b3e2af3184d8801fe90742f2e0c0a61
ikea1229 <11056019@ntub.edu.tw>
2024-01-03 18:47:28 +0800

    123

--- mysite/forms.py
+++ mysite/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
 
 class PostForm(forms.ModelForm):
     class Meta:
         model=Post
         fields=['booking']
+
+
+
+
+class CustomRegistrationForm(UserCreationForm):
+
+    pass
--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
 from mysite.models import Post
 from django.http import HttpResponse
 from datetime import datetime
 from django.shortcuts import redirect
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import CustomRegistrationForm
+
 
 
 # Create your views here.
@@ -26,3 +29,44 @@ def post_view(request,post_id):
 	post=Post.objects.get(id=post_id)
 	form=PostForm(instance=post)
 	return render(request,'post.html',{'post':post,'form':form})
+
+def index(request):
+    return render(request, 'accounts/index.html')
+
+
+#首頁
+def index(request):
+    return render(request, 'accounts/index.html')
+#註冊
+def sign_up(request):
+    form = UserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
+
+
+def custom_register(request):
+    if request.method == 'POST':
+        form = CustomRegistrationForm(request.POST)
+        if form.is_valid():
+            # 在這裡處理註冊邏輯
+            # 例如，創建新用戶，處理表單數據等
+            return redirect('home')  # 將 'home' 替換為你的首頁路徑
+    else:
+        form = CustomRegistrationForm()
+
+    return render(request, 'registration.html', {'form': form})
+
+# 註冊
+def sign_up(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login')  #重新導向到登入畫面
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
